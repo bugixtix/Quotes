69bc9cd413bafff5d61254f7832593881f3ec73b
Housam_ <ixtix_bug@hotmail.com>
2023-01-04 23:49:14 +0100

    " #04012023 init"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+export default function App() {
+
+  let [data_$, setData_$] = useState([])
+  let [ready_$, setReady_$] = useState(false)
+  let [count_$, setCount_$] = useState(0)
+  let [quote_$, setQuote_$] = useState([])
+
+  function count(e, count_){
+    
+    if(e.target.id==='left_'){
+      if(count_===0){
+        setCount_$(100)
+      }else{
+        setCount_$(prev=>prev-=1)
+      }
+    }
+    else if(e.target.id==='right_'){
+      if(count_===100){
+        setCount_$(0)
+      }else{
+        setCount_$(prev=>prev+=1)
+      }
+    }
+  }
+  
+  useEffect(()=>{
+    fetch('https://type.fit/api/quotes').then(res=>res.json()).then((response)=>{setData_$(response); setData_$(response); setReady_$(true)})
+    .catch(err=>console.log(err))
+  },[])
+  useEffect(()=>{
+    document.getElementById('left_').addEventListener('click',(e)=>count(e, count_$))
+    document.getElementById('right_').addEventListener('click',(e)=>count(e, count_$))
+  },[])
+  console.log(count_$)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='left_'></div>
+      {ready_$ && <SingleQuote quote={data_$[count_$].text} author={data_$[count_$].author}/>}
+      <div id='right_'></div>
     </div>
   );
 }
 
-export default App;
+function SingleQuote({quote,author}){
+
+  return(
+    <div id={'quote_'}>
+      <h3 id={'quote_body'}>"{quote}"</h3>
+      <h4 id={'quote_author'}>{author}</h4>
+    </div>
+  )
+}
